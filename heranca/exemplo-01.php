512e26bfc0d5cd7c0d84607c9282180779229ce2
tiago <tiagok989@gmail.com>
2021-11-09 07:17:30 -0300

    interface

--- heranca/exemplo-01.php
+++ heranca/exemplo-01.php
@@ -14,11 +14,21 @@ class Documento{
 
 class CPF extends Documento{
     public function validar():bool{
+
+        $numeroCPF = $this->getNumero();
         return true;
         //validação cpf
 
     }
 }
+
+$doc = new CPF();
+$doc->setNumero("12312312-21");
+$doc->validar();
+
+echo </br>;
+
+echo $doc->getNumero();
 
 
 ?>
