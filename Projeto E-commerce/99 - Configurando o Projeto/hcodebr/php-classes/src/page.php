bc5ccd63c06d3849dccffcc739d2530e5bc878e4
tiago <tiagok989@gmail.com>
2021-12-07 18:33:25 -0300

    projeto

---
+++ Projeto E-commerce/99 - Configurando o Projeto/hcodebr/php-classes/src/page.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+
+class Page{
+
+    public function __construct(){
+
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. "/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]. "/views-cache/"
+            "debug"         => true, // set to false to improve the speed
+        );
+
+    Tpl::configure( $config );
+
+    }
+
+    public function __destruct(){
+        
+    }
+
+    
+        
+    }
+
+
+
+?>
