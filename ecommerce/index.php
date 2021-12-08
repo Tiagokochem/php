a32d65921797960f386bf2f5d8e7c57854b4ef5c
tiago <tiagok989@gmail.com>
2021-12-08 08:18:32 -0300

    admin

---
+++ ecommerce/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use Hcode\Page;
+use Hcode\PageAdmin;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+
+	$page = new Page();
+
+	$page->setTpl("index");
+    
+	//$sql = new Hcode\DB\Sql();
+
+	//$results = $sql->select("SELECT * FROM tb_users");
+
+	//echo json_encode($results);
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+    
+});
+
+$app->run();
+
+ ?>
