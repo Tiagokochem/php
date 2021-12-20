de0e1a937a5afb0d2c38f566e7ada5b25659cc95
Tiago Kochem <57450432+Tiagokochem@users.noreply.github.com>
2021-12-20 20:21:26 -0300

    finais

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -1,51 +1,20 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Models\User;
+
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-
-	$page = new Page();
-
-	$page->setTpl("index");
-    
-	//$sql = new Hcode\DB\Sql();
-
-	//$results = $sql->select("SELECT * FROM tb_users");
-
-	//echo json_encode($results);
-
-});
-
-$app->get('/admin', function() {
-
-	$page = new PageAdmin();
-
-	$page->setTpl("index");
-    
-});
-
-$app->get('/login', function(){
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
-	$page->setTpl("login");
-});
-
-$app->post('/admin/login', function(){
-	User::login($_post["login"], $_POST["password"]);
-
-	header("Location: /admin");
-	exit;
-});
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
+require_once("admin-orders.php");
 
 $app->run();
 
